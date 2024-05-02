090fbf9c356788b138d861caf46c1ad75029d052
Gokul Menon <gokul.menon@free-now.com>
2024-05-02 16:03:16 +0200

    added blogs

---
+++ blogs/blogs.js
@@ -0,0 +1,15 @@
+// blogs.js
+const blogs = [
+    {
+      heading: "Appium — Dealing with FLAG_SECURE enabled in application",
+      link: "appium.html",
+      date: "January 1, 2022"
+    },
+    {
+      heading: "Heading - How do I edit $PATH (.bash_profile) on OSX?",
+      link: "bashprofile.html",
+      date: "January 1, 2022"
+    },
+
+  ];
+  
