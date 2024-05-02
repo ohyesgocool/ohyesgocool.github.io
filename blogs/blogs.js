08cc00ef9598466827b3d6487b9b1e7b12c25a5f
Gokul Menon <gokul.menon@free-now.com>
2024-05-02 18:10:18 +0200

    💻 added images

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -3,17 +3,20 @@ const blogs = [
    {
       heading: "Liquibase Tutorial",
       link: "liquibase.html",
-      date: "January 1, 2022"
+      date: "January 1, 2022",
+      numberOfWords : 900 
     },
     {
       heading: "Appium — Dealing with FLAG_SECURE enabled in application",
       link: "appium.html",
-      date: "January 1, 2022"
+      date: "January 1, 2022",
+      numberOfWords : 200 
     },
     {
       heading: "Heading - How do I edit $PATH (.bash_profile) on OSX?",
       link: "bashprofile.html",
-      date: "January 1, 2022"
+      date: "January 1, 2022",
+      numberOfWords : 200 
     },
 
   ];
